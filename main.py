2afe9dc8aacff89e6212ec4db4436dee3bca3258
tk <t.khamiam@gmail.com>
2025-07-04 11:39:37 +0700

    feat(api): /ft/latest route added

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from ft_scraper import scrape_ft_pea
+from datetime import datetime
+import pytz
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/ft/latest")
+def get_latest_ft():
+    ft_value = scrape_ft_pea()
+    if not ft_value:
+        return {"error": "FT value not found"}
+    return {
+        "ft_value": ft_value,
+        "scraped_at": datetime.now(pytz.timezone("Asia/Bangkok")).strftime("%Y-%m-%d %H:%M:%S"),
+    }
